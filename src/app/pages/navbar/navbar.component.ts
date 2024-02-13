e06c9a5cc7f19d8425e39643d778c3f85c6398e7
Gustavo Ferreira <gustavo.ferreira@tdm.local>
2024-02-13 15:13:29 -0300

    Perfilização Completa

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,5 +1,3 @@
-// NavbarComponent
-
 import { Component } from '@angular/core';
 import { NbMenuItem } from '@nebular/theme';
 import { AuthService } from 'src/app/services/Auth/authservice.service';
@@ -23,13 +21,13 @@ export class NavbarComponent {
       icon: 'eye-outline',
       children: [
         { title: 'Performance', link: '/dash-performance', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_PERFORMACE']) },
-        { title: 'Jurídico', link: '/dash-juridico', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'Contas a receber', link: '/dash-contas-a-receber', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'Emissão de Docs', link: '/dash-emissao-docs', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'Ocorrências', link: '/dash-ocorrencias', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'Logística', link: '/dash-logistica', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'Contas a Pagar', link: '/dash-contas-a-pagar', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
-        { title: 'GR', link: '/dash-gr', hidden: !this.authService.hasPermission(['ROLE_ADMIN']) },
+        { title: 'Jurídico', link: '/dash-juridico', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_JURIDICO']) },
+        { title: 'Contas a receber', link: '/dash-contas-a-receber', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_CONTASAREC']) },
+        { title: 'Emissão de Docs', link: '/dash-emissao-docs', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_EMISSAO']) },
+        { title: 'Ocorrências', link: '/dash-ocorrencias', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_OCORRENCIA']) },
+        { title: 'Logística', link: '/dash-logistica', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_LOGISTICA']) },
+        { title: 'Contas a Pagar', link: '/dash-contas-a-pagar', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_CONTASAPG']) },
+        { title: 'GR', link: '/dash-gr', hidden: !this.authService.hasPermission(['ROLE_ADMIN','ROLE_GR']) },
       ],
     },
     {
